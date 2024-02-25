8503a2f804a4c0300c5f8fd6ba58054c35a36abb
JGomersall98 <91595142+JGomersall98@users.noreply.github.com>
2024-02-25 19:28:12 +0000

    Uploaded Files Via Upload - Initial Build

---
+++ temperatureVariationAnalysis/Database/DB_Models/Fixture.cs
@@ -0,0 +1,12 @@
+﻿namespace temperatureVariationAnalysis.Database.DB_Models
+{
+	public class Fixture
+	{
+		public int FixtureId { get; set; }
+		public string City { get; set; }
+		public DateTime Date { get; set; }
+		public TimeSpan Time { get; set; }
+		public double Temperature { get; set; }
+		public ICollection<PlayerStat> PlayerStats { get; set; }
+	}
+}
