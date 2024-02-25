f8a682effa0781d1b38f1e32b79683ba0c0e9e9d
JGomersall98 <91595142+JGomersall98@users.noreply.github.com>
2024-02-25 23:07:20 +0000

    Add files via upload

---
+++ ControllerServices/UpdateDatabaseControllerService.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http.Headers;
+
+namespace MatchMasterWebAPI.ControllerServices
+{
+	public class UpdateDatabaseControllerService
+	{
+		public void UpdateDatabase()
+		{
+			var fixtureIdResponse = GetFixtureIdAsync();
+			
+		}
+
+		private async Task<object> GetFixtureIdAsync()
+		{
+
+			var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://api-football-v1.p.rapidapi.com/v3/fixtures?league=40&season=2023&team=64&status=FT"),
+				Headers =
+					{
+						{ "X-RapidAPI-Key", "bdc8d9557cmshf0bec4adac0297bp142c9djsn35ff23a72fb7" },
+						{ "X-RapidAPI-Host", "api-football-v1.p.rapidapi.com" },
+					},
+			};
+			using (var response = await client.SendAsync(request))
+			{
+				response.EnsureSuccessStatusCode();
+				var body = await response.Content.ReadAsStringAsync();
+				Console.WriteLine(body);
+				return body;
+			}
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Controllers/UpdateDatabaseController.cs
@@ -0,0 +1,21 @@
+﻿using MatchMasterWebAPI.ControllerServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchMasterWebAPI.Controllers
+{
+	[Route("api/v1/test")]
+	[ApiController]
+	public class UpdateDatabaseController : ControllerBase
+	{
+		[HttpPost]
+		public IActionResult Get()
+		{
+			//TODO: Add code to update the database
+			UpdateDatabaseControllerService updateDatabaseControllerService = new UpdateDatabaseControllerService();
+			updateDatabaseControllerService.UpdateDatabase();
+
+			return Ok();
+		}
+	}
+}
