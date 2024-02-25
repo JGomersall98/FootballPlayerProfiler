7c839066aa6a56e2d0f16f8bc014cb13ceb8381c
JGomersall98 <J.GOMERSALL-2021@hull.ac.uk>
2024-02-25 21:30:55 +0000

    New Build

---
+++ Database/DB_Models/Player.cs
@@ -0,0 +1,10 @@
+﻿namespace temperatureVariationAnalysis.Database.DB_Models
+{
+	public class Player
+	{
+		public int PlayerId { get; set; }
+		public string Name { get; set; }
+		public string Photo { get; set; }
+		public ICollection<PlayerStat> PlayerStats { get; set; }
+	}
+}
