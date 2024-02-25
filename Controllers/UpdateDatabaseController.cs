1032e1b0105b5544adad013cde2866fbed8badc2
JGomersall98 <J.GOMERSALL-2021@hull.ac.uk>
2024-02-25 21:45:40 +0000

    Edit

---
+++ Controllers/UpdateDatabaseController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchMasterWebAPI.Controllers
+{
+	[Route("api/v1/[controller]")]
+	[ApiController]
+	public class UpdateDatabaseController : ControllerBase
+	{
+		[HttpPost]
+		public IActionResult Get()
+		{
+			//TODO: Add code to update the database
+
+
+
+
+			return Ok();
+		}
+	}
+}
