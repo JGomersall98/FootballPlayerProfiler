6fc7a47657a8ba859a0f298f5892c4204c7485f2
JGomersall98 <J.GOMERSALL-2021@hull.ac.uk>
2024-02-28 11:52:09 +0000

    WorkingPlayerCardController

--- ControllerServices/In-DepthControllerServices/PlayerCardControllerService.cs
+++ ControllerServices/In-DepthControllerServices/PlayerCardControllerService.cs
@@ -1,5 +1,8 @@
-﻿using MatchMasterWEB.Database;
+﻿using MatchMasterWEB.ControllerServices.GetRatingMethods;
+using MatchMasterWEB.Database;
+using MatchMasterWEB.Database.DB_Models;
 using MatchMasterWEB.DTO_Models.SquadOverview;
+using Microsoft.EntityFrameworkCore;
 
 namespace MatchMasterWEB.ControllerServices.In_DepthControllerServices
 {
@@ -7,8 +10,28 @@ namespace MatchMasterWEB.ControllerServices.In_DepthControllerServices
 	{
 		public DTO_SquadOverviewPlayer GetPlayerCard(int playerId, MatchMasterMySqlDatabaseContext dbContext)
 		{
+			//Get player from database from playerId
+			Player? player = dbContext.Players
+				.Include(p => p.PlayerStats)
+				.Where(p => p.PlayerId == playerId)
+				.FirstOrDefault();
 
-			return new DTO_SquadOverviewPlayer { };
+			//If player is null return empty DTO
+			if (player == null)
+				return new DTO_SquadOverviewPlayer { };
+
+			GetPlayerAdaptabilityPercentage? _getPlayerAdaptabilityPercentage = new GetPlayerAdaptabilityPercentage();
+			GetPlayerAverageRating? _getPlayerAverageRating = new GetPlayerAverageRating();
+
+			return new DTO_SquadOverviewPlayer
+			{
+				PlayerId = player.PlayerId,
+				PlayerName = player.Name,
+				PlayerPhoto = player.Photo,
+				PlayerRating = _getPlayerAverageRating.GetAverageRating(player, dbContext),
+				AdaptabilityPercentage = _getPlayerAdaptabilityPercentage.GetAdaptabilityRating(player, dbContext)
+			};
+
 		}
 	}
 }
