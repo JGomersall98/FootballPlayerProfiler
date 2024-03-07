2de56e8ee0db07dd6680473860a2892d1ca7531d
JGomersall98 <J.GOMERSALL-2021@hull.ac.uk>
2024-03-07 21:59:01 +0000

    Created Tests for SquadOverviewControllerService

---
+++ MatchMaster-UnitTest/SquadOverviewControllerServiceTests/SquadOverviewControllerServiceTests.cs
@@ -0,0 +1,148 @@
+﻿using MatchMasterWEB.ControllerServices;
+using MatchMasterWEB.Database;
+using MatchMasterWEB.Database.DB_Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchMaster_UnitTest.SquadOverviewControllerServiceTests
+{
+    [TestClass]
+    public class SquadOverviewControllerServiceTests
+    {
+        private MatchMasterMySqlDatabaseContext? _mockDatabase;
+        [TestInitialize]
+        public void Initialize()
+        {
+            SquadOverviewControllerService_MockDatabase mockDatabase = new SquadOverviewControllerService_MockDatabase();
+            _mockDatabase = mockDatabase.GetMockDatabase();
+        }
+
+        [TestMethod]
+        public void GetSquadOverview_InvalidPosition_ThrowsArgumentException()
+        {
+            // Arrange
+            var service = new SquadOverviewControllerService();
+            Exception? caughtException = null;
+
+            // Act
+            try
+            {
+                service.GetSquadOverview("InvalidPosition", null!);
+            }
+            catch (Exception ex)
+            {
+                caughtException = ex;
+            }
+
+            // Assert
+            Assert.IsNotNull(caughtException); // Expected exception to be thrown
+            Assert.IsInstanceOfType(caughtException, typeof(ArgumentException)); // Expected exception to be of type ArgumentException
+        }
+        [TestMethod]
+        public void GetSquadOverview_ValidPosition_ReturnsList()
+        {
+            // Arrange
+            var service = new SquadOverviewControllerService();
+            var position = "F";
+
+            // Act
+            var result = service.GetSquadOverview(position, _mockDatabase!);
+
+            // Assert
+            Assert.IsNotNull(result); // Expected result to be not null
+            Assert.IsTrue(result.Players.Length > 0); // Expected at least one player
+            Assert.AreEqual(result.Players[0].PlayerName, "TestPlayer");
+        }
+        [TestMethod]
+        public void GetSquadOverview_NoPlayersForPosition_ReturnsEmptyList()
+        {
+            // Arrange
+            var service = new SquadOverviewControllerService();
+            var position = "M";
+
+            // Act
+            var result = service.GetSquadOverview(position, _mockDatabase!);
+
+            // Assert
+            Assert.IsNotNull(result); // Expected result to be not null
+            Assert.AreEqual(0, result.Players.Length); // Expected no players
+        }
+        [TestMethod]
+        public void GetSquadOverview_PlayersWithoutStats_ExcludedFromResults()
+        {
+            // Arrange
+            var service = new SquadOverviewControllerService();
+            var position = "M";
+
+            // Act
+            var result = service.GetSquadOverview(position, _mockDatabase!);
+
+            // Assert
+            Assert.IsNotNull(result); // Expected result to be not null
+                                      // Assert 
+            Assert.AreEqual(0, result.Players.Length); // Players without stats should be excluded
+        }
+        [TestMethod]
+        public void GetSquadOverview_CaseInsensitivePosition_ReturnsList()
+        {
+            // Arrange
+            var service = new SquadOverviewControllerService();
+            var position = "f";
+
+            // Act
+            var result = service.GetSquadOverview(position, _mockDatabase!);
+
+            // Assert
+            Assert.IsNotNull(result); // Expected result to be not null
+            Assert.IsTrue(result.Players.Length > 0); // Expected at least one player
+        }
+        [TestMethod]
+        public void GetSquadOverview_NullPosition_ThrowsArgumentException()
+        {
+            // Arrange
+            var service = new SquadOverviewControllerService();
+            Exception? caughtException = null;
+
+            // Act
+            try
+            {
+                service.GetSquadOverview(null!, _mockDatabase!);
+            }
+            catch (Exception ex)
+            {
+                caughtException = ex;
+            }
+
+            // Assert
+            Assert.IsNotNull(caughtException); // Expected exception to be thrown
+            Assert.IsInstanceOfType(caughtException, typeof(NullReferenceException)); // Expected exception to be of type ArgumentException
+        }
+        [TestMethod]
+        public void GetSquadOverview_PlayerRatingAdaptabilityAccuracy()
+        {
+            // Arrange
+            SquadOverviewControllerService_MockDatabase mockDatabase = new SquadOverviewControllerService_MockDatabase();
+            var _mockDatabase = mockDatabase.GetMockDatabase();
+
+            var service = new SquadOverviewControllerService();
+            var position = "D";
+
+            // Act
+            var result = service.GetSquadOverview(position, _mockDatabase);
+
+            // Assert
+            var testPlayer = result.Players.FirstOrDefault(p => p.PlayerName == "AdaptabilityTestPlayer");
+            Assert.IsNotNull(testPlayer); // Expected result to be not null
+
+            // Assuming expected adaptability is 93% and rating is 7.85 for the test player
+            Assert.AreEqual(93, testPlayer?.AdaptabilityPercentage?.AdaptabilityPercentage); 
+            Assert.AreEqual(7.85, testPlayer?.PlayerRating?.PlayerRating);
+        }
+
+    }
+
+}
---
+++ MatchMaster-UnitTest/SquadOverviewControllerServiceTests/SquadOverviewControllerService_MockDatabase.cs
@@ -0,0 +1,183 @@
+﻿using MatchMasterWEB.Database;
+using MatchMasterWEB.Database.DB_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchMaster_UnitTest.SquadOverviewControllerServiceTests
+{
+    [TestClass]
+    public class SquadOverviewControllerService_MockDatabase
+    {
+        private static SimulateMockDatabaseForUnitTests? _mockDatabase;
+
+        public MatchMasterMySqlDatabaseContext GetMockDatabase()
+        {
+            return _mockDatabase.GetContext();
+        }
+        public SquadOverviewControllerService_MockDatabase()
+        {
+            _mockDatabase = new SimulateMockDatabaseForUnitTests();
+
+            var testFixture = new Fixture
+            {
+                FixtureId = 1,
+                PlaceId = "TestPlace",
+                Date = new DateTime(2021, 1, 1),
+                Time = new TimeSpan(12, 0, 0),
+                Temperature = 20.0,
+                PlayerStats = new List<PlayerStat>
+                {
+                    new PlayerStat
+                    {
+                        StatId = 1,
+                        FixtureId = 1,
+                        Rating = 5.0,
+                        PassingAccuracy = 80,
+                        TotalTackles = 5,
+                        FoulsCommitted = 3,
+                        DuelsTotal = 10,
+                        DuelsWon = 5,
+                        DribblingAttempts = 10,
+                        DribblingSuccess = 5,
+                        TotalShots = 5,
+                        ShotsOnTarget = 3,
+                        Player = new Player
+                        {
+                            PlayerId = 1,
+                            Name = "TestPlayer",
+                            Photo = "TestPhoto",
+                            Position = "F",
+                        }
+                    }
+                }
+            };
+            _mockDatabase.Add(testFixture);
+
+            Player playerWithoutStats = new Player
+            {
+                PlayerId = 2,
+                Name = "TestPlayerWithoutStats",
+                Photo = "TestPhoto",
+                Position = "F",
+                PlayerStats = new List<PlayerStat>()
+            };
+            _mockDatabase.Add(playerWithoutStats);
+
+            //Fixture 2
+            var testFixture2 = new Fixture
+            {
+                FixtureId = 2,
+                PlaceId = "TestPlace",
+                Date = new DateTime(2021, 1, 1),
+                Time = new TimeSpan(12, 0, 0),
+                Temperature = 15.0
+            };
+            _mockDatabase.Add(testFixture2);
+            //Fixture 3
+            var testFixture3 = new Fixture
+            {
+                FixtureId = 3,
+                PlaceId = "TestPlace",
+                Date = new DateTime(2021, 1, 1),
+                Time = new TimeSpan(12, 0, 0),
+                Temperature = 10.0
+            };
+            _mockDatabase.Add(testFixture3);
+            //Fixture 4
+            var testFixture4 = new Fixture
+            {
+                FixtureId = 4,
+                PlaceId = "TestPlace",
+                Date = new DateTime(2021, 1, 1),
+                Time = new TimeSpan(12, 0, 0),
+                Temperature = 5.0
+            };
+            _mockDatabase.Add(testFixture4);
+            //Fixture 5
+            var testFixture5 = new Fixture
+            {
+                FixtureId = 5,
+                PlaceId = "TestPlace",
+                Date = new DateTime(2021, 1, 1),
+                Time = new TimeSpan(12, 0, 0),
+                Temperature = 0.0
+            };
+            _mockDatabase.Add(testFixture5);
+            //Fixture 6
+            var testFixture6 = new Fixture
+            {
+                FixtureId = 6,
+                PlaceId = "TestPlace",
+                Date = new DateTime(2021, 1, 1),
+                Time = new TimeSpan(12, 0, 0),
+                Temperature = -5.0
+            };
+            _mockDatabase.Add(testFixture6);
+            //Fixture 7
+            var testFixture7 = new Fixture
+            {
+                FixtureId = 7,
+                PlaceId = "TestPlace",
+                Date = new DateTime(2021, 1, 1),
+                Time = new TimeSpan(12, 0, 0),
+                Temperature = 10.0
+            };
+            _mockDatabase.Add(testFixture7);
+            //Fixture 8
+            var testFixture8 = new Fixture
+            {
+                FixtureId = 8,
+                PlaceId = "TestPlace",
+                Date = new DateTime(2021, 1, 1),
+                Time = new TimeSpan(12, 0, 0),
+                Temperature = 5.0
+            };
+            _mockDatabase.Add(testFixture8);
+            //Fixture 9
+            var testFixture9 = new Fixture
+            {
+                FixtureId = 9,
+                PlaceId = "TestPlace",
+                Date = new DateTime(2021, 1, 1),
+                Time = new TimeSpan(12, 0, 0),
+                Temperature = 13.0
+            };
+            _mockDatabase.Add(testFixture9);
+            //Fixture 10
+            var testFixture10 = new Fixture
+            {
+                FixtureId = 10,
+                PlaceId = "TestPlace",
+                Date = new DateTime(2021, 1, 1),
+                Time = new TimeSpan(12, 0, 0),
+                Temperature = 25.0
+            };
+            _mockDatabase.Add(testFixture10);
+
+            var playerStatsAccuracyPlayer = new Player
+            {
+                PlayerId = 3,
+                Name = "AdaptabilityTestPlayer",
+                Photo = "TestPhoto",
+                Position = "D",
+                PlayerStats = new List<PlayerStat>
+                {
+                    new PlayerStat { StatId = 2, FixtureId = 1, Rating = 8.5, PassingAccuracy = 90, TotalTackles = 10, FoulsCommitted = 2, DuelsTotal = 20, DuelsWon = 15, DribblingAttempts = 12, DribblingSuccess = 8, TotalShots = 10, ShotsOnTarget = 7 },
+                    new PlayerStat { StatId = 3, FixtureId = 2, Rating = 7.0, PassingAccuracy = 0, TotalTackles = 0, FoulsCommitted = 0, DuelsTotal = 20, DuelsWon = 15, DribblingAttempts = 12, DribblingSuccess = 8, TotalShots = 10, ShotsOnTarget = 7 },
+                    new PlayerStat { StatId = 4, FixtureId = 3, Rating = 9.0, PassingAccuracy = 0, TotalTackles = 0, FoulsCommitted = 0, DuelsTotal = 20, DuelsWon = 15, DribblingAttempts = 12, DribblingSuccess = 8, TotalShots = 10, ShotsOnTarget = 7 },
+                    new PlayerStat { StatId = 5, FixtureId = 4, Rating = 6.5, PassingAccuracy = 0, TotalTackles = 0, FoulsCommitted = 0, DuelsTotal = 20, DuelsWon = 15, DribblingAttempts = 12, DribblingSuccess = 8, TotalShots = 10, ShotsOnTarget = 7 },
+                    new PlayerStat { StatId = 6, FixtureId = 5, Rating = 8.0, PassingAccuracy = 0, TotalTackles = 0, FoulsCommitted = 0, DuelsTotal = 20, DuelsWon = 15, DribblingAttempts = 12, DribblingSuccess = 8, TotalShots = 10, ShotsOnTarget = 7 },
+                    new PlayerStat { StatId = 7, FixtureId = 6, Rating = 8.0, PassingAccuracy = 0, TotalTackles = 0, FoulsCommitted = 0, DuelsTotal = 20, DuelsWon = 15, DribblingAttempts = 12, DribblingSuccess = 8, TotalShots = 10, ShotsOnTarget = 7 },
+                    new PlayerStat { StatId = 8, FixtureId = 7, Rating = 9.0, PassingAccuracy = 0, TotalTackles = 0, FoulsCommitted = 0, DuelsTotal = 20, DuelsWon = 15, DribblingAttempts = 12, DribblingSuccess = 8, TotalShots = 10, ShotsOnTarget = 7 },
+                    new PlayerStat { StatId = 9, FixtureId = 8, Rating = 6.5, PassingAccuracy = 0, TotalTackles = 0, FoulsCommitted = 0, DuelsTotal = 20, DuelsWon = 15, DribblingAttempts = 12, DribblingSuccess = 8, TotalShots = 10, ShotsOnTarget = 7 },
+                    new PlayerStat { StatId = 10, FixtureId = 9, Rating = 8.0, PassingAccuracy = 0, TotalTackles = 0, FoulsCommitted = 0, DuelsTotal = 20, DuelsWon = 15, DribblingAttempts = 12, DribblingSuccess = 8, TotalShots = 10, ShotsOnTarget = 7 },
+                    new PlayerStat { StatId = 11, FixtureId = 10, Rating = 8.0, PassingAccuracy = 0, TotalTackles = 0, FoulsCommitted = 0, DuelsTotal = 20, DuelsWon = 15, DribblingAttempts = 12, DribblingSuccess = 8, TotalShots = 10, ShotsOnTarget = 7 }
+                }
+            };
+            _mockDatabase.Add(playerStatsAccuracyPlayer);
+        }
+    }
+}
